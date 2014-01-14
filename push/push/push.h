ecd1a0c67245a6111b5a11f1e7b5d8170b94fa61
Charlie <ankothari@gmail.com>
2014-01-14 22:19:57 +0530

    Initial Commit

---
+++ push/push/push.h
@@ -0,0 +1,13 @@
+//
+//  push.h
+//  push
+//
+//  Created by Apple on 14/01/14.
+//  Copyright (c) 2014 IdleCampus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface push : NSObject
+
+@end
---
+++ push/push/push.m
@@ -0,0 +1,13 @@
+//
+//  push.m
+//  push
+//
+//  Created by Apple on 14/01/14.
+//  Copyright (c) 2014 IdleCampus. All rights reserved.
+//
+
+#import "push.h"
+
+@implementation push
+
+@end
